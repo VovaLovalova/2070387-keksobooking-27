25ae4ab5e6b9e1587a97a12afe88aa4f53da9c9f
vova_lovalova <rudchenko.vova@mail.ru>
2022-10-13 20:50:28 +0800

    Рефакторинг функций рандомных чисел

--- js/main.js
+++ js/main.js
@@ -1,26 +1,23 @@
-function getRandomNumberInteger (minNumber, maxNumber) {
-  let min = Math.min(minNumber, maxNumber);
-  let max = Math.max(minNumber, maxNumber);
-  if (min >= 0 && max >= 0) {
-    min = Math.floor(min);
-    max = Math.floor(max);
+function getRandomPositiveInteger (minNumber, maxNumber) {
+  if (minNumber < 0 || maxNumber < 0) {
+    return NaN;
+  } else {
+    const min = Math.floor(Math.min(minNumber, maxNumber));
+    const max = Math.floor(Math.max(minNumber, maxNumber));
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else {
-    return NaN;
   }
 }
 
-console.log(getRandomNumberInteger(1.5, 1.9));
 
-
-function getRandomNumberFractional (minNumber, maxNumber) {
-  const min = Math.min(minNumber, maxNumber);
-  const max = Math.max(minNumber, maxNumber);
-  if (min >= 0 && max >= 0) {
+function getRandomPositiveFloat (minNumber, maxNumber) {
+  if (minNumber < 0 || maxNumber < 0) {
+    return NaN;
+  } else {
+    const min = Math.min(minNumber, maxNumber);
+    const max = Math.max(minNumber, maxNumber);
     return Math.random() * (max - min + 1) + min;
-  } else {
-    return NaN;
   }
 }
 
-console.log(getRandomNumberFractional(1.5,1.9));
+console.log(getRandomPositiveInteger(2.8, 8.8));
+console.log(getRandomPositiveFloat(2.8, 8.8));
